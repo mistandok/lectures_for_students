f7b2a1ac152eb578d73de4a0bcd8f1d2e04e4189
Anton Artikov <arti-anton@yandex.ru>
2024-09-14 09:25:15 +0300

    закончил со словорями

--- 2_lecture/dict_example/useful_example.py
+++ 2_lecture/dict_example/useful_example.py
@@ -1,19 +1,21 @@
 import json
 from collections import namedtuple
+from copy import copy, deepcopy  # noqa: F401
 from pprint import pprint
 from typing import TypeVar
 
 UserInfo = namedtuple("UserInfo", "age skills")
 UserID = TypeVar("UserID", bound=str)
 
-def get_users_info() -> dict[str, int]:
+
+def _get_users_info() -> dict[str, int]:
     # Предположим, что тут запрос на сервис user_info_service
     with open("./jsons/age.json", mode="r") as f:
         data = json.load(f)
         return data
 
 
-def get_users_skills() -> dict[str, list[str]]:
+def _get_users_skills() -> dict[str, list[str]]:
     # Предположим, что тут запрос на сервис user_skills_service
     with open("./jsons/skills.json", mode="r") as f:
         data = json.load(f)
@@ -21,8 +23,8 @@ def get_users_skills() -> dict[str, list[str]]:
 
 
 def collect_users() -> dict[UserID, UserInfo]:
-    user_info = get_users_info()
-    user_skills = get_users_skills()
+    user_info = _get_users_info()
+    user_skills = _get_users_skills()
 
     users = {}
     for user_id, age in user_info.items():
@@ -35,6 +37,59 @@ def collect_users() -> dict[UserID, UserInfo]:
     return users
 
 
+def merge_two_dicts():
+    common_users = {
+        "anton": 30,
+        "dima": 30,
+        "kolya": 40,
+        "vitya": 40,
+        "nastya": 20,
+        "vika": 60,
+    }
+
+    vip_users = {
+        "oleg": 40,
+        "nastya": 30,
+        "vika": 70,
+    }
+
+    new_users = {**common_users, **vip_users}
+    print(new_users)
+
+    common_users.update(vip_users)
+    print(common_users)
+
+
+def copy_dicts():
+    user_info = {
+        "name": "Vitya",
+        "age": 20,
+        "skills": ["go", "python"],
+        "other_info": {
+            "favorite_movie": "spider-man",
+            "favorite_cuisine": "italian",
+            "games_achievements_percents": {
+                "cyberpunk": 50,
+            }
+        }
+    }
+
+    copy_user_info = copy(user_info)
+    # copy_user_info = deepcopy(user_info)
+
+    print("copied object")
+    pprint(copy_user_info, width=1)
+    print("deep change in copied object")
+    copy_user_info["other_info"]["games_achievements_percents"]["cyberpunk"] = 100
+    print("one level change")
+    copy_user_info["age"] = 40
+
+    print("results:")
+    print("copy")
+    pprint(copy_user_info, width=1)
+    print("original")
+    pprint(user_info, width=1)
+
+
 if __name__ == '__main__':
-    users = collect_users()
-    pprint(users, width=1)
+    copy_dicts()
